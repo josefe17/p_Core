16ac6bd69ccfdd99d8fbb28096de462abb1fc243
josefe <josefe17@gmail.com>
2016-05-19 22:12:39 +0200

    Edición de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,7 +7,7 @@ exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId)
   		.then(function(quiz) {
       		if (quiz) {
-        		req.quiz = quiz;
+        		req.quiz = quiz; //Lo mete en req.quiz
         		next();
       		} else { 
       			next(new Error('No existe quizId=' + quizId));
@@ -84,6 +84,37 @@ exports.show = function(req, res, next) {
 	res.render('quizzes/show', {quiz: req.quiz,
 								answer: answer});
 };
+
+// GET /quizzes/:id/edit
+exports.edit=function(req, res, next){
+	var quiz = req.quiz;
+	res.render('quizzes/edit', {quiz: quiz});
+}
+
+//PUT /quizzes/:id
+
+exports.update =function(req, res, next){
+	req.quiz.question = req.body.quiz.question;
+	req.quiz.answer   = req.body.quiz.answer;
+
+	req.quiz.save({fields: ["question", "answer"]})
+	.then(function(quiz){
+		req.flash('success', 'Quiz editado correctamente');
+		res.redirect("/quizzes"); // Redirección a la lista de preguntas
+	})
+	.catch(Sequelize.ValidationError, function(error) {
+
+      req.flash('error', 'Errores en el formulario:');
+      for (var i in error.errors) {
+          req.flash('error', error.errors[i].value);
+      };
+      res.render('quizzes/edit', {quiz: quiz}); 
+      })
+    .catch(function(error) {
+		req.flash('error', 'Errores al editar un quiz');
+		next(error);
+	});  
+}
 
 
 // GET /quizzes/:id/check
